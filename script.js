155ff317cc1d55fc8c671352a3c733ba033832df
Karen10Ruiz <163039934+Karen10Ruiz@users.noreply.github.com>
2024-12-18 17:02:13 -0600

    Programamos la funcionalidad de cambio de color

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-alert ("EXAMEN")
+//alert ("EXAMEN")
+
+/*Traer refrencias al parrafo y botón */
+
+const button = document.getElementById("button"); 
+const color = document.getElementById("color");
+
+/* Manejar el evento */
+
+button.addEventListener( "click", cambiarColor );
+
+/* Definir la función */
+
+function cambiarColor() {
+ /* Código que se ejecuta */
+
+// Función anidada
+
+function colores() {
+     /* Código que se ejecuta */
+    const opciones = "0123456789abcdef";
+    let colorhex = "#"; 
+
+    /* Recorrer el string opciones */
+
+    for (let i = 0 ; i < 6 ; i++) {
+        let indiceAleatorio = Math.floor(Math.random() * 16);
+        colorhex += opciones[indiceAleatorio]; // #000000
+    }
+
+    return colorhex; 
+}
+
+  let colorAleatorio = colores(); //Actualiza el texto
+  color.textContent = colorAleatorio; //actualiza el color de fondo
+  document.body.style.backgroundColor = colorAleatorio
+ 
+}
